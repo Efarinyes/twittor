8abbe078490223f50e2257d59c985e0148fe2dac
Efarinyes <efarinyes@gmail.com>
2019-10-09 20:14:56 +0200

    Modificacions necessaries per dispositius Iphone - IOS

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ importScripts('js/sw-utils.js');
 
 // Declarem les constants on s'instalaran les diferents caches
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v3';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
